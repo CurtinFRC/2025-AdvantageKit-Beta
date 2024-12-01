511614be50e4c303533d0eef1aa16051eebe9795
Jade Turner <spacey-sooty@proton.me>
2024-12-01 19:55:06 +0800

    oops double file
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- src/main/java/frc/robot/generated/BuildConstants.java
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -1,21 +1,19 @@
-// Copyright (c) 2024 CurtinFRC
-// Open Source Software, you can modify it according to the terms
-// of the MIT License at the root of this project
+package frc.robot.generated;
 
-package frc.robot;
-
-/** Automatically generated file containing build version information. */
+/**
+ * Automatically generated file containing build version information.
+ */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 3;
-  public static final String GIT_SHA = "a19e6abe401ffc7d43c87ddc67ae93211b27a8ab";
-  public static final String GIT_DATE = "2024-12-01 19:28:10 AWST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "5542e7a11ba779bcf8c49dc1435ac6cbda61816a";
+  public static final String GIT_DATE = "2024-12-01 19:47:37 AWST";
   public static final String GIT_BRANCH = "master";
-  public static final String BUILD_DATE = "2024-12-01 19:33:24 AWST";
-  public static final long BUILD_UNIX_TIME = 1733052804994L;
+  public static final String BUILD_DATE = "2024-12-01 19:54:54 AWST";
+  public static final long BUILD_UNIX_TIME = 1733054094608L;
   public static final int DIRTY = 1;
 
-  private BuildConstants() {}
+  private BuildConstants(){}
 }
